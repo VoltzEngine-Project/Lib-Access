1f490648150ef1ea2166ee606a962d95a1ec31a1
Robert Seifert <rseifert.phone@gmail.com>
2017-04-26 14:04:24 -0400

    Added a username field and add user button to group users gui

--- src/main/scala/com/builtbroken/mc/framework/access/gui/frame/group/user/GuiFrameGroupUsers.java
+++ src/main/scala/com/builtbroken/mc/framework/access/gui/frame/group/user/GuiFrameGroupUsers.java
@@ -3,8 +3,13 @@ package com.builtbroken.mc.framework.access.gui.frame.group.user;
 import com.builtbroken.mc.framework.access.AccessUser;
 import com.builtbroken.mc.framework.access.gui.GuiAccessSystem;
 import com.builtbroken.mc.framework.access.gui.frame.group.GuiGroupFrame;
+import com.builtbroken.mc.framework.access.gui.packets.PacketAccessGui;
+import com.builtbroken.mc.prefab.gui.GuiButton2;
 import com.builtbroken.mc.prefab.gui.components.GuiArray;
+import com.builtbroken.mc.prefab.gui.components.GuiField;
 import com.builtbroken.mc.prefab.gui.pos.GuiRelativePos;
+import com.builtbroken.mc.prefab.gui.pos.HugBottom;
+import net.minecraft.client.gui.GuiButton;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -16,6 +21,7 @@ public class GuiFrameGroupUsers extends GuiGroupFrame<GuiFrameGroupUsers>
     public static int rows = 15;
 
     public GuiArray groupArray;
+    public GuiField userNameField;
     public String[] users;
 
     public GuiFrameGroupUsers(GuiAccessSystem parent, String groupID, int id, int x, int y)
@@ -35,10 +41,35 @@ public class GuiFrameGroupUsers extends GuiGroupFrame<GuiFrameGroupUsers>
             {
                 users[i++] = user.getName();
             }
-            groupArray = add(new GuiArray(new UserArrayCallback(this), 5, 0, 0, rows, rowSpacingY));
+            groupArray = add(new GuiArray(new UserArrayCallback(this), 1, 0, 0, rows, rowSpacingY));
             groupArray.setRelativePosition(new GuiRelativePos(this, 0, 20));
             groupArray.setWidth(200);
+
+            userNameField = add(new GuiField(0, 0));
+            userNameField.setRelativePosition(new HugBottom(this, 1, -21, true));
+            userNameField.setWidth(100);
+
+            GuiButton2 addButton = add(new GuiButton2(2, 0, 0, "Add"));
+            addButton.setWidth(50);
+            addButton.setRelativePosition(new HugBottom(this, -addButton.getWidth(), -20, false));
         }
         updatePositions();
+    }
+
+    @Override
+    public void actionPerformed(GuiButton button)
+    {
+        int id = button.id;
+        if(id == 2)
+        {
+            if(userNameField.getText() != null && !userNameField.getText().isEmpty())
+            {
+                PacketAccessGui.addUser(getHost().currentProfile.getID(), groupID, userNameField.getText());
+            }
+        }
+        else
+        {
+            super.actionPerformed(button);
+        }
     }
 }
